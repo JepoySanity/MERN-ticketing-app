76e571f5c2997d269ba3b4430cc8bc4c2bb319b1
Jepoy Factoran <factoran20@gmail.com>
2022-06-19 18:04:17 +0800

    added create ticket api resource

--- backend/controller/ticketController.js
+++ backend/controller/ticketController.js
@@ -6,16 +6,44 @@ const Ticket = require("../models/ticketsModel");
 // @desc    Get user tickets
 // @route   GET /api/tickets
 // @access  private
-const getTickets = (req, res) => {
-  res.status(200).json({ message: "get ticket" });
-};
+const getTickets = asyncHandler(async (req, res) => {
+  // get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("user not found");
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+  res.status(200).send(tickets);
+});
 
 // @desc    Create new ticket
 // @route   POST /api/tickets
 // @access  private
-const createTicket = (req, res) => {
-  res.status(200).json({ message: "create ticket" });
-};
+const createTicket = asyncHandler(async (req, res) => {
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error("Please add a product and description");
+  }
+
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("user not found");
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: "new",
+  });
+
+  res.status(201).json(ticket);
+});
 
 module.exports = {
   getTickets,
