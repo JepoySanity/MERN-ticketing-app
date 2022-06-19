e6b73dda7a172a1678b62f17f103eb9851307d90
Jepoy Factoran <factoran20@gmail.com>
2022-06-19 08:17:58 +0800

    hook login and registration form to authSlice

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../features/auth/authSlice";
 function Login() {
   const [formData, setFormData] = useState({
     email: "",
@@ -16,9 +18,21 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const userData = {
+      email,
+      password,
+    };
+    dispatch(login(userData));
   };
 
   const { email, password } = formData;
+
+  const dispatch = useDispatch();
+
+  const { user, isLoading, iSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
   return (
     <>
       <section className="heading">
