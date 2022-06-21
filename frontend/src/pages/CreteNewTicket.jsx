01d06697a66099aa1bd6bf20e741f5c70c7c6712
Jepoy Factoran <factoran20@gmail.com>
2022-06-21 20:16:43 +0800

    added customer name and email pre-filled input on create ticket form

--- frontend/src/pages/CreteNewTicket.jsx
+++ frontend/src/pages/CreteNewTicket.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
 function CreteNewTicket() {
-  return <div>CreteNewTicket</div>;
+  const { user } = useSelector((state) => state.auth);
+  const [name] = useState(user.name);
+  const [email] = useState(user.email);
+  const [product, setProduct] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <>
+      <section className="heading">
+        <h1>Create New Ticket</h1>
+        <p>Please fill out the form below</p>
+      </section>
+      <section className="form">
+        <div className="form-group">
+          <label htmlFor="name">Customer Name</label>
+          <input type="text" className="form-control" value={name} disabled />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Customer Email</label>
+          <input type="text" className="form-control" value={email} disabled />
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default CreteNewTicket;
