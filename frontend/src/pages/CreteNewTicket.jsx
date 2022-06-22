81d94f42c61282232b5c75cdcddef30a92023db0
Jepoy Factoran <factoran20@gmail.com>
2022-06-22 20:34:01 +0800

    added back button component

--- frontend/src/pages/CreteNewTicket.jsx
+++ frontend/src/pages/CreteNewTicket.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { createTicket, reset } from "../features/tickets/ticketSlice";
 import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
 
 function CreteNewTicket() {
   const { user } = useSelector((state) => state.auth);
@@ -40,6 +41,7 @@ function CreteNewTicket() {
 
   return (
     <>
+      <BackButton url="/" />
       <section className="heading">
         <h1>Create New Ticket</h1>
         <p>Please fill out the form below</p>
